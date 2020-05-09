e0ea8e9f6fc6093a512f6d32f28b500954235026
Carlos Segura <romheat@gmail.com>
2020-05-09 17:49:56 +0200

    samples

--- sample/sample.js
+++ sample/sample.js
@@ -1,36 +1,31 @@
-const portera = require('../lib/portera');
+const portera = require("../lib/portera");
 
-portera('http://localhost:3001', console);
-
-//console.log('init');
-//console.log('sample');
+portera("http://localhost:3001", console);
 
 const o = {
-  a: 'test',
-  b: 'test2',
+  a: "test",
+  b: "test2",
   c: 123,
 };
 
 try {
-  throw new Error('Super Error');
+  throw new Error("Super Error");
 } catch (err) {
   console.error(err);
 }
 
-console.assert(o.a === 'sample', 'o.a not is sample', o);
-
+console.assert(o.a === "sample", "o.a not is sample", o);
+console.trace();
 console.log(o);
 console.log(process.platform);
 console.log(1234);
-console.info('sample', __dirname);
-console.log('object', o, o);
+console.info("sample", __dirname);
+console.log("object", o, o);
 console.warn(1, 2, 3, 4);
-console.log('uno', 'dos', 'tres');
-console.log('uno', 'dos', o);
+console.log("uno", "dos", "tres");
+console.log("uno", "dos", o);
 console.error(null);
-console.error('test');
-
-console.trace('Test', 'we are here');
+console.error("test");
 
 function add(a, b) {
   console.stack();
@@ -44,7 +39,8 @@ function re_add(a, b) {
 add(1, 2);
 re_add(55, 55);
 
-console.assert(1 == 2, 'not the same', '1 === 2');
+console.assert(1 == 2, "not the same", "1 === 2");
+console.trace("Test", "we are here");
 
-console.log('exit');
+console.log("exit");
 setTimeout(() => process.exit(), 2000);
